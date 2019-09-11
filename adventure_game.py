3142233199577a81ac4753614a6faee4ec58dc73
noha2 <54306051+noha2@users.noreply.github.com>
2019-09-11 05:21:59 +0200

    Update adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -1,76 +1,87 @@
+#the interface of game
 import time
-#the interface og  thre game
-print("your adventure game start...destroy them")
-time.sleep(3)
-print("are you ready hero ?!...gooo")
-time.sleep(3)
 
 import random
+places=["the old palace","under thr ground"]
 
-places=["old palace","under the ground"]
+weapons=["sword","rope","dart"]
 
-evils=["monker","titan","forester"]
+evils=["titan","forester","monkey"]
 
-weapons=["dart","rope","sword"]
+place=random.choice(places)
 
-place = random.choice(places)
+weapon=random.choice(weapons)
 
-evil= random.choice(evils)
+evil=random.choice(evils)
 
-weapon= random.choice(weapons)
-#my game details
-def printsos(sos):
-    print(sos)  
-    time.sleep(2)
+#my game deteails
+def printsos(message):
+    print(message)
+    time.sleep(3)
+
+def replay():
+    replay = input("Wanna play again ?\n"
+                   "Please anwser with y or n.\n")
+    if replay == "y":
+        field()
+    elif replay == "n":
+        return(printsos("OK, goodbye."))
+    else:
+        printsos("Please anwser with y or n.")
+        replay()
+ 
+ def field():
+     choice = int(input("Enter 1 to go to the palace.\n"
+                       "Enter 2 to peer into the cave.\n"
+                       "Please enter 1 or 2.\n"))
+     if choice == 1:
+        palace()
+     elif choice == 2:
+         cave()
+     else:
+         printsos("Please enter 1 or 2.\n")
+         field()
+
 
 def cave():
-    printsos("oh no it is dark here .what will you do ")
-    printsos("you are in a game gyu and you can click on the conor of screen..do not worry!")
-    printsos("it is bright now .so take a deep breath and and move on forward to the X")
-    printsos("please,far all these things and take it"+weapon)
-    printsos("you are ready now to the next step .do not turn out .look under the X there is a tunnel ,go and star\n go hero go ")
-    tunnel()
-def field():
-    printsos("there are  strane plants hete , why it look like that")
-    printsos("look there is a path and you should go right")
-    printsos("look there is a black tree; behind it there is X and box t,take it " +weapon ) 
-    printsos("you are warrior now, go not left there are a wooden door it is lead you to tunnel go..")
-    tunnel()
+printsos(" ohh no,it is dark.\n i tell you inneed lentern !......\n what will you do now ?!   ")
+printsos("you are in a game guy and you can click o on the corner of your screen do not warry...?!" )
+printsos("look, it it bright . yeah now take a deep breath and movo on forward to thr X ")
+printsos(" start far all things in the ground . there is a surprise here ")
+printsos("now take it" + weapon)
+printsos("you are ready now to the next step.do not turu out look under the X there is a tunnel ti go to in and palace.......\n go hero...")
+field()
+def palace():
+  printsos("the on fire now,are you ready or what. all the way fired  ")
+  printsos("do not escape hero i tell you bafore it is a game and joking with you(:")
+  printsos(evil+ "what the hell, it is big .the war is start . let is destroy them  ")
+  optios=int(input("your optios 1 or 2 have not third!\n"
+                  "1.start the war\n"
+                  "2.end the gamr\n"))
+  if optios == 1:
+     printsos("you are hero") 
+     printsos("choose it now"+weapon)
+  if optios ==2:
+     printsos("i think it is the wrong choice")   
+     turnchoice=int(input("your choice 1 to complete or 2 \n"
+                         "1.yes\n"
+                         "2.no\n"
+    ))
+     if turnchoice==1:
+        palace()
+     elif turnchoice==2:
+          gamrover()
+def gamrover()  
+    printsos("thanks.for playing") 
+    replay()
 def start():
-    start=int(input("your time to win come.first step ,,choose the place click 1 to inter to faild ,2 to inter to the cave"))
-    if start == 1:
-       field()
-    elif start== 2:
-         cave()
-    else:
-        printsos("please choose 1 or 2 only")
-def tunnel():
-    printsos("it is on fire now, are you ready or what.all the way fired")
-    printsos("do not escape baby it it a game i tell you before . i am joking with you !(:")
-    printsos(evil+"what the hell .it is big .the war is star lets destroy them")
-    options=int(input("your option 1 or 2 have not third!\n"
-    "1.star the war\n"
-    "2.end the game\n"))
-    if options ==1: 
-       printsos("you are a hero go")
-       start()  
-    if options== 2:
-       printsos("i think it is the wrong choice")
-       turnchoice()   
-def turnchoice():
-    turnchoice=int(input("chose 1 to complete\n 
-    2 to end the game\n"))
-    if turnchoice == 1:
-       start()
-    if turnchoice == 2:
-       gameover() 
-def gameover():
-    printsos("thanks for paying")                       
-def play():
-    printsos("you are in the gammmme"+place)
-    start()
+    printsos("your adventure start ... destroy him" + place)
+    printsos("are you ready hero?! gooooo...!")
+    printsos("from where you will start")
+    field()
 
-play()
+start()
+         
 
 
 
